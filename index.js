cfca9cddcea9abcbf756d49793e91f2ec086f6c7
109062126 <ssuyung@gapp.nthu.edu.tw>
2022-04-01 13:58:07 +0800

    finished brush and eraser thickness

---
+++ index.js
@@ -0,0 +1,78 @@
+// var c = document.getElementById("myCanvas");
+// var ctx = c.getContext("2d");
+// ctx.moveTo(0, 0);
+// ctx.lineTo(200, 100);
+// ctx.stroke();
+var cur_x, cur_y, prev_x, prev_y,
+    mouse_down = false, 
+    thickness = 50;
+    canvas = document.getElementById('myCanvas'),
+    ctx = canvas.getContext("2d"),
+    cur_function="pen";
+function init() {
+    // cur_x = cur_y = prev_x = prev_y = 0;
+    // mouse_down = false;
+    // console.log("check");
+    
+    // ctx = canvas.getContext("2d");
+   
+
+    canvas.addEventListener("mousemove", function (e) {
+        findxy('move', e)
+    }, false);
+    canvas.addEventListener("mousedown", function (e) {
+        findxy('down', e)
+    }, false);
+    canvas.addEventListener("mouseup", function (e) {
+        findxy('up', e)
+    }, false);
+    canvas.addEventListener("mouseout", function (e) {
+        findxy('out', e)
+    }, false);
+}
+function findxy(movement, e){
+    if(movement == "move"){
+        prev_x = cur_x;
+        prev_y = cur_y
+        cur_x = e.clientX - canvas.offsetLeft;
+        cur_y = e.clientY - canvas.offsetTop;
+        if(mouse_down){
+            if(cur_function=="pen") draw();
+            else if(cur_function=="eraser") erase();
+        }
+    } else if(movement == "down"){
+        mouse_down = true;
+        prev_x = cur_x;
+        prev_y = cur_y
+        cur_x = e.clientX - canvas.offsetLeft;
+        cur_y = e.clientY - canvas.offsetTop;
+        console.log(cur_x, cur_y);
+    } else if(movement == "up"){
+        mouse_down = false;
+    } else if(movement == "out"){
+
+    }
+}
+function draw(){
+    var color = document.getElementById("colorpicker");
+    console.log("color = "+color.value);
+    ctx.beginPath();
+    ctx.moveTo(prev_x, prev_y);
+    ctx.lineTo(cur_x, cur_y);
+    ctx.strokeStyle = color.value;
+    ctx.lineWidth = thickness*0.2;
+    ctx.stroke();
+    ctx.closePath();
+}
+function clickbutton(item){
+    cur_function=item;
+    console.log("cur_function:" + item);
+}
+function erase(){
+    ctx.clearRect(cur_x-thickness*0.25, cur_y-thickness*0.25, thickness*0.5, thickness*0.5);
+}
+function changeThickness(){
+    var slider=document.getElementById("thickness");
+    thickness = slider.value;
+    console.log("Change thickness to " + thickness);
+}
